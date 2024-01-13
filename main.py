edefeeea1defb7d3e63f6cad54c0f32593c8a7c2
Tony Arkaysia <111191587+tonyArkaysia@users.noreply.github.com>
2024-01-13 13:25:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/tester')
 def tester():
     return redirect('https://tonyarkaysia.github.io/isl-cybergateway')
 
