a209d50c908e716af1e67b3107a7a1bbd352817b
Tony Arkaysia <111191587+tonyArkaysia@users.noreply.github.com>
2024-01-13 13:09:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def tester():
-    return redirect('https://tonyarkaysia.github.io/isl-cybergateway')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # This block of code executes when a POST request is made (typically when submitting a form)
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == "prachya" and password == "calilove":
+            # Redirect to an external URL (in this case, a personal profile page)
+            return redirect('https://tonyarkaysia.github.io/isl-profile')
+        else:
+            # If the provided credentials are incorrect, redirect to a retry login page
+            return redirect('https://tonyarkaysia.github.io/isl-retry-login')
+    
+    # This part of the code executes for a GET request or when the form is initially loaded
+    # It redirects to a retry login page since no login attempt has been made yet
+    return redirect('https://tonyarkaysia.github.io/isl-retry-login') # Render a login template here
 
 
 if __name__ == '__main__':
