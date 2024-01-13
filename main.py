72509757c0fe7c538a5c74c3b32e104e4b06c7cf
Tony Arkaysia <111191587+tonyArkaysia@users.noreply.github.com>
2024-01-13 13:15:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,7 +21,6 @@ def login():
     # This part of the code executes for a GET request or when the form is initially loaded
     # It redirects to a retry login page since no login attempt has been made yet
     return redirect('https://tonyarkaysia.github.io/isl-retry-login') # Render a login template here
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
