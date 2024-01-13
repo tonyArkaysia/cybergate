a541a9e3f885cfd20ca63e2962149a8cc03df6b3
Tony Arkaysia <111191587+tonyArkaysia@users.noreply.github.com>
2024-01-13 13:22:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 from flask import Flask, request, redirect, render_template
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # This block of code executes when a POST request is made (typically when submitting a form)
-        username = request.form['username']
-        password = request.form['password']
-
-        if username == "prachya" and password == "calilove":
-            # Redirect to an external URL (in this case, a personal profile page)
-            return redirect('https://tonyarkaysia.github.io/isl-profile')
-        else:
-            # If the provided credentials are incorrect, redirect to a retry login page
-            return redirect('https://tonyarkaysia.github.io/isl-retry-login')
-    
-    # This part of the code executes for a GET request or when the form is initially loaded
-    # It redirects to a retry login page since no login attempt has been made yet
-    return redirect('https://tonyarkaysia.github.io/isl-retry-login') # Render a login template here
+@app.route('/')
+def tester():
+    return redirect('https://tonyarkaysia.github.io/isl-cybergateway')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
