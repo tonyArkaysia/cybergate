ab0d98689c806ed9c032f7ef49e3fe1c86399b42
Tony Arkaysia <111191587+tonyArkaysia@users.noreply.github.com>
2024-01-13 13:00:52 -0500

    Update main.py
    app update

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def tester():
+    return redirect('https://tonyarkaysia.github.io/isl-cybergateway')
 
 
 if __name__ == '__main__':
