749f17736ebd70a55ea653837c3d8660d4048aeb
Tony Arkaysia <111191587+tonyArkaysia@users.noreply.github.com>
2024-01-13 23:24:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 from flask import Flask, request, redirect
+import psycopg2
+import os
+from config import access_cred
+import bcrypt
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 
+# Database connection setup
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        port=os.getenv('DB_PORT')
+    )
+    return conn
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # This block of code executes when a POST request is made (typically when submitting a form)
         username = request.form['username']
-        password = request.form['password']
+        password = request.form['password'].encode('utf-8')  # Ensure password is in bytes
 
-        if username == "prachya" and password == "californialove":
-            # Redirect to an external URL (in this case, a personal profile page)
+        # Connect to the DB
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Query to retrieve the user's stored hash
+        cur.execute('SELECT password_hash FROM customer WHERE username = %s', (username,))
+        user = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        if user and bcrypt.checkpw(password, user[0].encode('utf-8')):  # Check the provided password against the stored hash
             return redirect('https://tonyarkaysia.github.io/isl-profile')
         else:
-            # If the provided credentials are incorrect, redirect to a retry login page
             return redirect('https://tonyarkaysia.github.io/isl-retry-login')
-    
-    # This part of the code executes for a GET request or when the form is initially loaded
-    # It redirects to a retry login page since no login attempt has been made yet
-    return redirect('https://tonyarkaysia.github.io/isl-retry-login') # Render a login template here
+
+    return redirect('https://tonyarkaysia.github.io/isl-retry-login')
 
 @app.route('/add-api-key', methods=['POST'])
 def add_api_key():
@@ -33,4 +53,7 @@ def add_api_key():
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+
+
+
 
